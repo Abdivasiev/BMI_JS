ce47658484de763bc8cd587207cb51fdf1c19c68
Abdivasiev <abdivasiev@gmail.com>
2021-11-12 15:13:09 +0600

    If statements usage

--- bmi_counter.js
+++ bmi_counter.js
@@ -5,3 +5,9 @@ function bmiFormula(m, h) {
 let firstPerson = bmiFormula(110,1.75);
 let secondPerson = bmiFormula(85,1.76);
 console.log(firstPerson, secondPerson, firstPerson > secondPerson);
+if (firstPerson>secondPerson){
+   alert(`Firsts's BMI is higher than Seconds's! by ${firstPerson-secondPerson} score!`)
+}
+if (secondPerson>firstPerson){
+    alert(`2's BMI is higher than 1's! by ${secondPerson-firstPerson} score`)
+}
